03faff0264fbf398222d73b8dc66fa712028d9ed
PhilippOesch <philipp.oeschger@hs-furtwangen.de>
2020-04-28 18:17:40 +0200

    Snake with loop

--- Snake/main.js
+++ Snake/main.js
@@ -1,17 +1,18 @@
 "use strict";
-var Snake;
-(function (Snake) {
+var SnakeGame;
+(function (SnakeGame) {
     var ƒ = FudgeCore;
     window.addEventListener("load", hndload);
     let viewport; //viewport variable
+    let snakeScene = new ƒ.Node("SnakeScene");
     let snake = new ƒ.Node("Snake");
+    snake.addComponent(new ƒ.ComponentTransform());
     function hndload(_event) {
         const canvas = document.querySelector("canvas");
-        //ƒ.RenderManager.initialize();
+        ƒ.RenderManager.initialize();
         ƒ.Debug.log(canvas);
         let mesh = new ƒ.MeshQuad(); //Create a Quad Mesh
         let mtrSolidWhite = new ƒ.Material("SolidGreen", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("green")));
-        //THe Node
         //let node: ƒ.Node= new ƒ.Node("Quad"); //Node for our Object
         for (let i = 0; i < 4; i++) {
             let node = new ƒ.Node("Quad"); //Node for our Object
@@ -24,23 +25,22 @@ var Snake;
             // node.mtxLocal.scale(ƒ.Vector3.ONE(0.8));
             snake.appendChild(node);
         }
-        // //The Mesh
-        // let mesh: ƒ.MeshCube= new ƒ.MeshCube(); //Create a Quad Mesh
-        // let cmpMesh: ƒ.ComponentMesh= new ƒ.ComponentMesh(mesh); //attache Mesh to Node
-        // node.addComponent(cmpMesh); //Add Component into node component Map
-        // //The Material
-        // let mtrSolidWhite: ƒ.Material= new ƒ.Material("SolidGreen", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("green")));
-        // let cmpMesh: ƒ.ComponentMesh= new ƒ.ComponentMesh(mesh); //attache Mesh to Node
-        // node.addComponent(cmpMesh); //Add Component into node component Map
+        snakeScene.addChild(snake);
         //The Camera
         let cmpCamera = new ƒ.ComponentCamera();
-        cmpCamera.pivot.translateZ(10);
+        cmpCamera.pivot.translateZ(30);
         cmpCamera.pivot.rotateY(180);
         //The Viewport
         viewport = new ƒ.Viewport();
-        viewport.initialize("Viewport", snake, cmpCamera, canvas);
+        viewport.initialize("Viewport", snakeScene, cmpCamera, canvas);
         ƒ.Debug.log(viewport);
         viewport.draw();
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 10);
     }
-})(Snake || (Snake = {}));
+    function update() {
+        snake.cmpTransform.local.translate(new ƒ.Vector3(1, 0, 0));
+        viewport.draw();
+    }
+})(SnakeGame || (SnakeGame = {}));
 //# sourceMappingURL=main.js.map
--- Snake/main.ts
+++ Snake/main.ts
@@ -1,20 +1,24 @@
-namespace Snake {
+namespace SnakeGame {
     import ƒ= FudgeCore;
 
     window.addEventListener("load", hndload)
 
     let viewport: ƒ.Viewport;    //viewport variable
 
+    let snakeScene: ƒ.Node= new ƒ.Node("SnakeScene")
+
     let snake: ƒ.Node= new ƒ.Node("Snake")
+    snake.addComponent(new ƒ.ComponentTransform());
 
     function hndload(_event: Event):void {
         const canvas: HTMLCanvasElement= document.querySelector("canvas");
-        //ƒ.RenderManager.initialize();
+
+        ƒ.RenderManager.initialize();
         ƒ.Debug.log(canvas);
 
         let mesh: ƒ.MeshQuad= new ƒ.MeshQuad(); //Create a Quad Mesh
         let mtrSolidWhite: ƒ.Material= new ƒ.Material("SolidGreen", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("green")));
-        //THe Node
+
         //let node: ƒ.Node= new ƒ.Node("Quad"); //Node for our Object
         for(let i= 0; i< 4; i++){
             let node: ƒ.Node= new ƒ.Node("Quad"); //Node for our Object
@@ -28,30 +32,29 @@ namespace Snake {
 
             node.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(new ƒ.Vector3(-1*i,0,0))));
             // node.mtxLocal.scale(ƒ.Vector3.ONE(0.8));
-
             snake.appendChild(node);
         }
-
-        // //The Mesh
-        // let mesh: ƒ.MeshCube= new ƒ.MeshCube(); //Create a Quad Mesh
-        // let cmpMesh: ƒ.ComponentMesh= new ƒ.ComponentMesh(mesh); //attache Mesh to Node
-        // node.addComponent(cmpMesh); //Add Component into node component Map
-        
-        // //The Material
-        // let mtrSolidWhite: ƒ.Material= new ƒ.Material("SolidGreen", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("green")));
-        // let cmpMesh: ƒ.ComponentMesh= new ƒ.ComponentMesh(mesh); //attache Mesh to Node
-        // node.addComponent(cmpMesh); //Add Component into node component Map
+        snakeScene.addChild(snake);
 
         //The Camera
         let cmpCamera: ƒ.ComponentCamera= new ƒ.ComponentCamera();
-        cmpCamera.pivot.translateZ(10);
+        cmpCamera.pivot.translateZ(30);
         cmpCamera.pivot.rotateY(180);
 
         //The Viewport
         viewport= new ƒ.Viewport();
-        viewport.initialize("Viewport", snake, cmpCamera, canvas);
+        viewport.initialize("Viewport", snakeScene, cmpCamera, canvas);
         ƒ.Debug.log(viewport);
 
         viewport.draw();
+
+        ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_GAME, 10);
     }
+
+    function update(){
+        snake.cmpTransform.local.translate(new ƒ.Vector3(1,0,0));
+        viewport.draw();
+    }
+
 }
