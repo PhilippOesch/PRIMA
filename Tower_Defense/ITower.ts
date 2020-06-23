9bfdce3fc3141c4796cddba4cdd5cec992f5d2ff
PhilippOesch <49947694+PhilippOesch@users.noreply.github.com>
2020-06-23 16:27:34 +0200

    merge with dev

--- Tower_Defense/ITower.ts
+++ Tower_Defense/ITower.ts
@@ -5,7 +5,7 @@ namespace TowerDefense {
     import ƒAid = FudgeAid;
 
     export class ITower extends Tower {
-        shootingInterval2: number;
+        shootingInterval2: NodeJS.Timeout;
         isShooting2: boolean = false;
         cannon1RelPos: ƒ.Vector3;
         cannon2RelPos: ƒ.Vector3;
--- Tower_Defense/Tower.ts
+++ Tower_Defense/Tower.ts
@@ -9,7 +9,7 @@ namespace TowerDefense {
 
         protected position: ƒ.Vector3;
         protected rate: number = 1000; //in ms
-        protected shootingInterval: number;
+        protected shootingInterval: NodeJS.Timeout;
         protected isShooting: boolean = false;
         protected range: number = 14;
         protected targetedEnemy: Enemy;
